f5d17b91f5888c05d50352ddab0293d7207ade76
S1yGus <holy-fox@mail.ru>
2024-04-27 00:38:52 +0300

    migrate to Unreal Engine 5.4

--- Source/Cube_Game.Target.cs
+++ Source/Cube_Game.Target.cs
@@ -12,7 +12,7 @@ public class Cube_GameTarget : TargetRules
     public Cube_GameTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Game;
-        DefaultBuildSettings = BuildSettingsVersion.V4;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
         ExtraModuleNames.AddRange(new string[] { "Cube_Game" });
